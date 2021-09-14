cdee861311043a8a40e13ffd50b7aebd0b2a1c84
Jefitoifto <jeferson.santos4@estudante.ifto.edu.br>
2021-09-14 12:07:47 -0300

    aula7 postada

---
+++ Programação Imperativa/aula14-1.js
@@ -0,0 +1,7 @@
+let soma = require('./aula14');
+let divisao = require('./aula14');
+let multiplica = require('./aula14');
+
+//require('./aula14').divisao(7,7);
+require('./aula14').multiplica(7,7);
+require('./aula14').soma(7,7);
---
+++ Programação Imperativa/aula14.js
@@ -0,0 +1,17 @@
+module.exports = {
+    soma: function (a, b) {
+      console.log(a + b);
+    },
+  };
+
+  module.exports = {
+    divisao: function (a, b) {
+      console.log(a / b);
+    },
+  };
+
+  module.exports = {
+    multiplica: function (a, b) {
+      console.log(a * b);
+    },
+  };
