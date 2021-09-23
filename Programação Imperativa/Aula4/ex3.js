12ae771099e5e6a5d4d7997d81a6a357014b530c
Jefitoifto <jeferson.santos4@estudante.ifto.edu.br>
2021-09-23 16:20:52 -0300

    ext3 aula4

---
+++ Programação Imperativa/Aula4/ex3.js
@@ -0,0 +1,20 @@
+let n1 = 25;
+let n2 = 35;
+
+let res = n1 + n2;
+
+console.log(res);
+
+const nome = "Ricardo";
+const sexo = "Masculino";
+
+var num1 = 10;
+var num2 = 20;
+
+var num3 = num1 + num2;
+
+console.log("Nome: " + nome);
+console.log("Sexo: " + sexo);
+console.log("Nota: " + (num3 + res));
+
+console.log("Re-atribuindo valor +1 em n1 que é 25: " + (n1 + 1));
